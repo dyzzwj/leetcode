8a823cafa668ef4d280047a2f8330f0686b4f725
dyzzwj <dyzwj1998@163.com>
2023-09-13 20:53:10 +0800

    排序

--- src/main/java/com/dyzwj/sort/插入排序.java
+++ src/main/java/com/dyzwj/sort/插入排序.java
@@ -9,9 +9,27 @@ package com.dyzwj.sort;
 public class 插入排序 extends Sort{
 
 
+    public static void main(String[] args) {
+
+        int[] arr = initArr(10);
+        printArr(arr);
+        sort(arr);
+        System.out.println();
+        printArr(arr);
+
+    }
+
 
     public static void sort(int[] arr){
-
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+  1; j > 0 ; j--) {
+                if(arr[j] < arr[j-1]){
+                    int temp  = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
 
     }
 }
